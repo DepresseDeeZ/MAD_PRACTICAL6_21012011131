a5f0c61fdd397355eed4c7ec495ed5e726f27ab8
student <student@gnu.ac.in>
2023-08-22 10:18:36 +0530

    final tues

--- app/src/main/java/com/example/mad_practical6_21012011131/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical6_21012011131/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mad_practical6_21012011131
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,4 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+//    var mediaPlayer = MediaPlayer.create(context, R.raw.sound_file_1)
+//    mediaPlayer.start() // no need to call prepare(); create() does that for you
+//}
 }
