bc201ed66e316095b0d55a1f92cce6c6e619f8e1
student <student@gnu.ac.in>
2023-09-11 09:53:18 +0530

    services created

--- app/src/main/java/com/example/mad_practical6_21012011131/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical6_21012011131/MainActivity.kt
@@ -1,15 +1,41 @@
 package com.example.mad_practical6_21012011131
 
+import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val play: FloatingActionButton = findViewById(R.id.floatingActionButton);
+        val stop: FloatingActionButton = findViewById(R.id.floatingActionButton5);
+
+        play.setOnClickListener {
+            playbtn();
+        }
+        stop.setOnClickListener {
+            stopbtn();
+        }
+
+        }
+    fun playbtn() {
+        Intent(applicationContext, MyService::class.java).putExtra(
+            MyService.PLAYERKEY,
+            MyService.PLAYERVALUE
+        ).apply { startService(this) }
     }
-//    var mediaPlayer = MediaPlayer.create(context, R.raw.sound_file_1)
-//    mediaPlayer.start() // no need to call prepare(); create() does that for you
-//}
+
+    fun stopbtn() {
+        Intent(applicationContext, MyService::class.java).putExtra(
+            MyService.PLAYERKEY,
+            MyService.PLAYERVALUE
+        ).apply { stopService(this) }
+    }
 }
+
+
